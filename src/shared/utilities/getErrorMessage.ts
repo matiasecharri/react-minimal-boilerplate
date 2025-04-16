7e4b7d4b862f758ff0d516db598b8d9336b6ceff
matiasecharri <matiasecharri@hotmail.com>
2025-04-16 03:44:43 -0300

    feat: add getErrorMessage utility to map error messages to user-friendly text

---
+++ src/shared/utilities/getErrorMessage.ts
@@ -0,0 +1,9 @@
+export const getErrorMessage = (error: unknown) => {
+  if (!(error instanceof Error)) return "An unknown error occurred.";
+
+  const message = error.message.toLowerCase();
+
+  if (message.includes("404")) return "Content not found.";
+  if (message.includes("500")) return "Server error.";
+  return "An unexpected error occurred.";
+};
