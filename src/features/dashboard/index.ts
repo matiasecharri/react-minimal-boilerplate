dcc9faceaa4dc6357ac6fb0f79256ef503e79d3c
matiasecharri <matiasecharri@hotmail.com>
2025-04-17 00:18:59 -0300

    chore: add barrel export for dashboard feature

--- src/features/dashboard/index.ts
+++ src/features/dashboard/index.ts
@@ -1,6 +1,1 @@
-// features/dashboard/index.ts
-
-/*export * from './components/DashboardPanel/DashboardPanel';
-export * from './components/DashboardItem/DashboardItem';
-export * from './hooks/useDashboardData';
-export * from './models/dashboard.models';*/ // Opcional, si necesitás exponer tipos
+export * from "./components";
