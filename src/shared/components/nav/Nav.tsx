b0aefe114d505507340485d6b7afa46bb2ce6830
matiasecharri <matiasecharri@hotmail.com>
2025-04-23 21:46:38 -0300

    feat: add useNavigate as example for redirection after login

--- src/shared/components/nav/Nav.tsx
+++ src/shared/components/nav/Nav.tsx
@@ -1,19 +1,26 @@
 import { AppRoutes } from "@/shared/routes";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { useUser } from "@/shared/context/user";
 import { Button } from "../button";
-import s from "./nav.module.css";
 import clsx from "clsx";
+import s from "./nav.module.css";
 
 export const Nav = () => {
   const { user, toggleLogged } = useUser();
+  const navigate = useNavigate();
   const btnText = user.isLogged ? "Sign Out" : "Sign In";
   const btnStyles = clsx(s.btn, user.isLogged ? s.signout : s.signin);
+
+  const handleSignIn = () => {
+    user.isLogged
+      ? toggleLogged()
+      : (toggleLogged(), navigate(AppRoutes.private.DASHBOARD));
+  };
 
   return (
     <nav className={s.nav}>
       <Button
-        handleClick={toggleLogged}
+        handleClick={handleSignIn}
         text={btnText}
         ariaLabel={btnText}
         className={btnStyles}
